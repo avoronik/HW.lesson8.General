344c4e78fd5dde815f6112e2f35cf785980ad3b4
Anastasia Voronik <avoronik@icloud.com>
2024-02-28 01:58:15 +0300

    commit1.2 - full.dirty

--- HW.lesson8.General.playground/Contents.swift
+++ HW.lesson8.General.playground/Contents.swift
@@ -128,24 +128,162 @@ myWebsite.enterYourPersonalInfo()
  */
 
 class Human {
-    var weaponDamage: Int
-    var health: Int
-    var armour: Int
-    var evasion: Int
+    var type: String
+    var name: String
+    var weaponDamage: Int    // урон
+    var health: Int         // здоровье
+    var armour: Int        // броня
+    var evasion: Bool     // уклонение
     
-    init(weaponDamage: Int, health: Int, armour: Int, evasion: Int) {
+    init(type: String = "Человек", name: String, weaponDamage: Int = 4, health: Int = 9, armour: Int = 5, evasion: Bool = false) {
+        self.type = type
+        self.name = name
         self.weaponDamage = weaponDamage
         self.health = health
         self.armour = armour
         self.evasion = evasion
     }
+    // при атаке (здоровье атакуемого - урон атакующего, дополнительно учесть уклонение и броню
+    func description() {
+        print("Класс персонажа: \(type), имя: \(name), здоровье: \(health), наносимый урон: \(weaponDamage), броня: \(armour), уклонение: \(evasion)")
+    }
+    func attacks( attaked:(Human), _ attacking:(Human)) {
+        //        var restHealth = (attaked.health + attaked.armour) - attacking.weaponDamage
+        //        var restHealth2 = (attacking.health + attacking.armour) - attaked.weaponDamage
+        //        var restHealth3 = restHealth - attacking.weaponDamage
+        //        var restHealth4 = restHealth2 - attaked.weaponDamage
+        if attaked.health > 0 {
+            if attaked.evasion == true {
+                print("\(attacking.name) промахивается. Сработало уклонение от первой атаки. Урон не нанесён.")
+                attaked.evasion = false
+            }
+            if attaked.evasion == false {
+                //            var totalHealth = attaked.armour + attaked.health
+                //            var restHealth = totalHealth - attacking.weaponDamage
+                if attaked.armour > 0 {
+                    attaked.armour -= attacking.weaponDamage
+                    print("Количество брони персонажа \(attaked.name) : \(attaked.armour), оставшиеся жизни \(attaked.health)")
+                    print("Количество брони персонажа \(attacking.name) : \(attacking.armour), оставшиеся жизни \(attacking.health) ")
+                } else {
+                    attaked.armour = 0
+                    attaked.health -= attacking.weaponDamage
+                    print("Количество жизней персонажа \(attaked.name) : \(attaked.health), осталось брони: \(attaked.armour) ")
+                    print("Количество жизней персонажа \(attacking.name) : \(attacking.health), осталось брони: \(attacking.armour) ")
+                }
+                //            restHealth -= attacking.weaponDamage
+                
+                
+                //            while attaked.health >= 0 && attacking.health >= 0 {
+                //                print("\(attacking.name) атакует \(attaked.name)")
+                
+            }
+            
+            
+            //                print(" после атаки у \(attaked.name) осталось \(restHealth) жизней ")
+            //                if restHealth < 0 {
+            //                    print("\(attaked.name) потерпел поражение")}
+            //
+            //                print("\(attaked.name) атакует \(attacking.name)")
+            //                print(" после атаки у \(attacking.name) осталось \(restHealth2) жизней ")
+            //        }
+            //                 if restHealth2 < 0 {
+            //                 print("\(attacking.name) потерпел поражение")
+            //         }
+            //                    }
+            //               print("Турнир завершен")
+        }
+    }
 }
+    
+  
+//        if restHealth <= 0 {
+//            print ("\(attaked.name) побежден")
+//            print("Победу одержал \(attacking.name)")
+//        }
+//        if restHealthSecondAttack <= 0 {
+//            print ("\(attacking.name) побежден")
+//            print("Победу одержал \(attaked.name)")
+//        }
+
+
+
 
 class Orc: Human {
-     
+    override init(type: String = "Орк", name: String, weaponDamage: Int = 3, health: Int = 10, armour: Int = 10, evasion: Bool = false) {
+        super.init(type: type, name: name, weaponDamage: weaponDamage, health: health, armour: armour, evasion: evasion )
+    }
+    override func description() {
+        print("Класс персонажа: \(type), имя: \(name), здоровье: \(health), наносимый урон: \(weaponDamage), броня: \(armour), уклонение: \(evasion)")
+    }
 }
+     
+
 
 class Elf: Human {
-    
+   
+    override init(type: String = "Эльф", name: String, weaponDamage: Int = 6, health: Int = 8, armour: Int = 0, evasion: Bool = true) {
+        super.init(type: type, name: name, weaponDamage: weaponDamage, health: health, armour: armour, evasion: evasion)
+    }
+    override func description() {
+        print("Класс персонажа: \(type), имя: \(name), здоровье: \(health), наносимый урон: \(weaponDamage), броня: \(armour), уклонение: \(evasion)")
+    }
 }
 
+var human: Human = Human(name: "Мишель")
+var orc: Orc = Orc(name: "Азор")
+var elf: Elf = Elf(name: "Дессида")
+
+func AllCharactersDescription(){
+   print("Список участников турнира:")
+    print("1. Класс: \(human.type), имя: \(human.name)")
+    print("2. Класс: \(orc.type) , имя: \(orc.name)")
+    print("3. Класс: \(elf.type) , имя: \(elf.name)")
+}
+
+ AllCharactersDescription()
+
+
+
+extension Human {
+    func fight(attacking: Human, attaked: Human) {
+        print("\(attacking.name) вызывает на бой \(attaked.name)")
+        print("Информация о бойцах:")
+        print(attacking.description())
+        print(attaked.description())
+        print("Бой начинается")
+        while attaked.health >= 0 && attacking.health >= 0 {
+            print("\(attacking.name) атакует \(attaked.name) и наносит \(attacking.weaponDamage) урона")
+            attacking.attacks(attaked: attaked, attacking)
+           // print("Количество жизней персонажа \(attaked.name) : \(attaked.health)")
+            
+            if attaked.health <= 0 {
+                print("Победу одержал \(attacking.name)")
+                break
+            }
+            print("\(attaked.name) ответно атакует \(attacking.name) и наносит \(attaked.weaponDamage) урона")
+            attaked.attacks(attaked: attacking, attaked)
+          //  print("Количество жизней персонажа \(attacking.name) : \(attacking.health)")
+            
+            if attacking.health <= 0 {
+                print("Победу одержал \(attaked.name)")
+                
+                break
+            }
+        }
+        print("Бой окончен")
+    }
+}
+
+print("Раунд 1.")
+
+elf.fight(attacking: elf, attaked: human)
+
+print("Раунд 2.")
+
+human.fight(attacking: human, attaked: orc)
+
+print("Раунд 3.")
+
+orc.fight(attacking: orc, attaked: human)
+
+print("Турнир завершён")
